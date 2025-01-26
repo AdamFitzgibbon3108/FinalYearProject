bc5f6757bd4d1ea599087761db7c83a50789b69b
AdamFitzgibbon3108 <148885141+AdamFitzgibbon3108@users.noreply.github.com>
2025-01-26 17:10:04 +0000

    Create ScoringService.java

---
+++ ScoringService.java
@@ -0,0 +1,31 @@
+package com.example.service;
+
+import com.example.model.Response;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ScoringService {
+
+    public int calculateScore(List<Response> responses) {
+        int score = 0;
+        for (Response response : responses) {
+            if ("Yes".equalsIgnoreCase(response.getAnswer())) {
+                score++;
+            }
+        }
+        return score;
+    }
+
+	public void saveResponses(Map<String, String> responses) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int calculateScore(Map<String, String> responses) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
