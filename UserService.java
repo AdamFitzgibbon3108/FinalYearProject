f70ecaedfa3b162d7d6b1a0ba0d178ad401ab219
AdamFitzgibbon3108 <148885141+AdamFitzgibbon3108@users.noreply.github.com>
2025-01-27 14:21:05 +0000

    Update UserService.java

--- UserService.java
+++ UserService.java
@@ -1,17 +1,14 @@
 package com.example.service;
 
 import com.example.model.User;
-import com.example.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 public interface UserService {
     List<User> getAllUsers();
+    User getUserById(Long id);
     User createUser(User user);
     User updateUser(Long id, User user);
     void deleteUser(Long id);
-	
+	User findByUsername(String username);
 }
 
